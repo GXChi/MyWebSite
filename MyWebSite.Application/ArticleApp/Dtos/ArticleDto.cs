af3ec252d87917573ccfaac211b5d3545acff882
郭校池 <577597095@qq.com>
2018-07-04 17:34:17 +0800

    修改文章界面

--- MyWebSite.Application/ArticleApp/Dtos/ArticleDto.cs
+++ MyWebSite.Application/ArticleApp/Dtos/ArticleDto.cs
@@ -7,6 +7,7 @@ namespace MyWebSite.Application.ArticleApp.Dtos
 {
     public class ArticleDto
     {
+        public Guid id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public string Url { get; set; }
